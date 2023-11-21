017a2d41c4446b67d7ca0fe71a446e391d0bf6f2
alinojomi <anojomi3@gmail.com>
2023-11-21 15:58:28 +0330

    Changed sideNav bg-color

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -31,19 +31,16 @@ export default function NavLinks() {
                     <Link
                         key={link.name}
                         href={link.href}
-                        className={clsx(
-                            'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-                            {
-                                'bg-sky-100 text-blue-600': pathname === link.href,
-                            },
-                        )}
+                        className={
+                            `flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-blue-600 hover:text-[white] md:flex-none md:justify-start md:p-2 md:px-3 ${pathname === link.href && `bg-blue-600 text-[white]`}`
+                        }
                     >
                         <LinkIcon className="w-6"/>
                         <p className="hidden md:block">{link.name}</p>
                     </Link>
                 );
             })}
-</>
-)
-;
+        </>
+    )
+        ;
 }
